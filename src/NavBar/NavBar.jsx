61f30119106f17301037d0748bb961139c83a363
kevin5112 <kevin5112@yahoo.com>
2024-04-12 15:31:48 -0700

    set up homepage and routing

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div className="nav-bar-root">
+      <div>Home</div>
+      <div>Shop</div>
+    </div>
+  );
+}
+
+export default NavBar;
